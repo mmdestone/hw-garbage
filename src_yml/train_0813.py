90a575e542bc101a55607d8849fe98d4289742ab
yml <y@yml.red>
2019-08-13 23:09:40 +0800

    bug fix

--- src_yml/train_0813.py
+++ src_yml/train_0813.py
@@ -19,42 +19,60 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from keras.applications.inception_v3 import InceptionV3, preprocess_input
+from keras.applications.xception import Xception
+from keras.applications.resnet50 import ResNet50
 from keras.models import Model, Sequential, Input
 from keras.layers import *
+from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint, EarlyStopping
 import os
 import PIL
 import time
 
 # os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-
+# %%
+path_data = 'E:/garbage_classify/train_data'
+batch_size = 32
+img_width = 224
+img_height = 224
+random_seed = 1000
 
 # %%
-path_data = 'E:/garbage_classify/train_data/'
+labels_file = 'tmp/labels.csv'
+try:
+    labels = pd.read_csv(labels_file)
+except FileNotFoundError as e:
+    print(e)
+    labels = glob(f'{path_data}/*.txt')
+    labels = pd.concat([pd.read_csv(label_f, header=None)
+                        for label_f in labels])
+    labels.columns = ['fname', 'label']
+    labels.to_csv(labels_file, index=None)
 
-
-# %%
-labels = glob(f'{path_data}/*.txt')
-labels = pd.concat([pd.read_csv(label_f, header=None) for label_f in labels])
-
-
-# %%
-labels.columns = ['fname', 'label']
 labels.label = labels.label.apply(str)
 labels.head()
-
-
 # %%
-ig = ImageDataGenerator(
-    preprocessing_function=preprocess_input, validation_split=0.2)
+labels.groupby(by='label').count().plot(style='.')
+# %%
+ig = ImageDataGenerator(validation_split=0.2,
+                        preprocessing_function=utils.preprocess_img
+                        )
 
-batch_size = 64
+params_g = dict(batch_size=batch_size,
+                directory=path_data,
+                # class_mode='other',
+                x_col='fname',
+                y_col='label',
+                target_size=(img_width, img_height),
+                seed=random_seed)
 
 train_g = ig.flow_from_dataframe(
-    labels, path_data, batch_size=batch_size, x_col='fname', y_col='label', target_size=(224, 224), subset='training')
+    labels, subset='training', **params_g)
 valid_g = ig.flow_from_dataframe(
-    labels, path_data, batch_size=batch_size, x_col='fname', y_col='label', target_size=(224, 224), subset='validation')
-
+    labels, subset='validation', **params_g)
+# %%
+(imgs, lbs) = next(train_g)
+imgs.shape, lbs.shape
 
 # %%
 n_classess = labels.label.unique().shape[0]
@@ -62,22 +80,25 @@ n_classess
 
 
 # %%
-base_model = InceptionV3(weights='imagenet', include_top=False)
+# base_model = InceptionV3(
+#     weights='imagenet', include_top=False, input_shape=(img_width, img_height, 3))
 # base_model = InceptionV3(weights=None, include_top=False)
-
+base_model = ResNet50(
+    weights='imagenet', include_top=False, input_shape=(img_width, img_height, 3))
 # 添加全局平均池化层
 x = base_model.output
 x = GlobalAveragePooling2D()(x)
 
 # 添加一个全连接层
-x = Dense(1024, activation='relu')(x)
+# x = Dense(512, activation='relu')(x)
+# x = Dense(128, activation='relu')(x)
 
 # 添加一个分类器，假设我们有200个类
 predictions = Dense(n_classess, activation='softmax')(x)
 
 # 构建我们需要训练的完整模型
 model = Model(inputs=base_model.input, outputs=predictions)
-
+# model.summary()
 # 首先，我们只训练顶部的几层（随机初始化的层）
 # 锁住所有 InceptionV3 的卷积层
 for layer in base_model.layers:
@@ -89,7 +110,7 @@ estop = EarlyStopping(monitor='val_acc', min_delta=1e-7,
                       verbose=1, patience=20)
 
 # %%
-model.compile(optimizer='adam', loss='categorical_crossentropy',
+model.compile(optimizer=Adam(lr=1e-4), loss='categorical_crossentropy',
               metrics=['accuracy'])
 model.fit_generator(
     train_g,
--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 # %%
 from tensorflow.keras.applications.vgg16 import preprocess_input
 
-def preprocess_img(x, **kwargs):
-    preprocess_input(x, mode='tf', **kwargs)
+def preprocess_img(x):
+    return preprocess_input(x, mode='tf')
 
 
 #%%
